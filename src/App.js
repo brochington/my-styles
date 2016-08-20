0180aaf19988863401de865c2b2af1fde32b5b84
Broch Stilley <broch@lumi.com>
2016-08-19 23:28:21 -0700

    Adding Heading, margin, padding styles.

--- src/App.js
+++ src/App.js
@@ -5,14 +5,35 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
-        <h4>Heading 4</h4>
-        <h5>Heading 5</h5>
-        <h6>Heading 6</h6>
-        <div>
+
+        <div className="h1 larger">Heading 1, larger</div>
+        <div className="h1">Heading 1</div>
+        <div className="h1 smaller">Heading 1, smaller</div>
+
+        <div className="h2 larger">Heading 2, larger</div>
+        <div className="h2">Heading 2</div>
+        <div className="h2 smaller">Heading 2, smaller</div>
+
+        <div className="h3 larger">Heading 3, larger</div>
+        <div className="h3">Heading 3</div>
+        <div className="h3 smaller">Heading 3, smaller</div>
+
+        <div className="h4 larger">Heading 4, larger</div>
+        <div className="h4">Heading 4</div>
+        <div className="h4 smaller">Heading 4, smaller</div>
+
+        <div className="h5 larger">Heading 5, larger</div>
+        <div className="h5">Heading 5</div>
+        <div className="h5 smaller">Heading 5, smaller</div>
+
+        <div className="h6 larger">Heading 6, larger</div>
+        <div className="h6">Heading 6</div>
+        <div className="h6 smaller">Heading 6, smaller</div>
+        <div className="column mb1">
+            <span className="larger">This is a large span</span>
             <span>This is a span</span>
+            <span className="smaller">This is a small span</span>
+
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import App from './App';
 import './index.css';
 
 /* my css files */
+import './css/basics.css';
 import './css/heading.css';
+import './css/flex.css';
+import './css/margin.css';
+import './css/padding.css';
 
 ReactDOM.render(
   <App />,
