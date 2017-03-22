2b74fb61d35de15794198311f53d5cbcb0a8e229
Broch Stilley <brochington@gmail.com>
2017-03-21 23:58:34 -0700

    Updates to forms and buttons

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -5,14 +5,14 @@ class ButtonPage extends React.Component {
         return (
             <div className="column justify-around flex-wrap mt2">
               <div className="row flex-wrap">
-                <button className="btn">Default</button>
-                <button className="btn disabled">disabled Default</button>
-                <button className="btn btn-primary">Primary</button>
-                <button className="btn btn-success">Success</button>
-                <button className="btn btn-info">Info</button>
-                <button className="btn btn-warning">Warning</button>
-                <button className="btn btn-danger">Danger</button>
-                <button className="btn btn-disabled">Disabled</button>
+                <button className="btn m1">Default</button>
+                <button className="btn m1 disabled">disabled Default</button>
+                <button className="btn m1 btn-primary">Primary</button>
+                <button className="btn m1 btn-success">Success</button>
+                <button className="btn m1 btn-info">Info</button>
+                <button className="btn m1 btn-warning">Warning</button>
+                <button className="btn m1 btn-danger">Danger</button>
+                <button className="btn m1 btn-disabled">Disabled</button>
               </div>
 
             </div>
--- src/pages/FormsPage.js
+++ src/pages/FormsPage.js
@@ -3,16 +3,51 @@ import React from 'react'
 class FormsPage extends React.Component {
     render() {
         return (
-            <div className="column">
-                <div className="column">
-                    <label htmlFor="text1">Text 1</label>
-                    <input type="text" id="text1" />
-                </div>
-                <div className="column">
-                    <label htmlFor="textarea1">Text Area 1</label>
-                    <input type="textarea" id="textarea1" />
-                </div>
-            </div>
+            <form>
+                <fieldset>
+                    <legend className="h4 mb2">Test Form 1</legend>
+                    <div className="column vw50">
+                        <div className="column">
+                            <label htmlFor="text1">Text 1</label>
+                            <input type="text" id="text1" />
+                        </div>
+                        <div className="row">
+                            <div className="column mr2">
+                                <label htmlFor="text2">Text 2</label>
+                                <input type="text" id="text2" />
+                            </div>
+                            <div className="column">
+                                <label htmlFor="text3">Text 3</label>
+                                <input type="text" id="text3" />
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="row align-center w50">
+                                <label className="w30" htmlFor="text4">Text 4</label>
+                                <input type="text" id="text4" className="w50"/>
+                            </div>
+                            <div className="row align-center w50">
+                                <label className="w30" htmlFor="text5">Text 5</label>
+                                <input type="text" id="text5" className="w50"/>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="row align-center w50">
+                                <label className="w30" htmlFor="text4">Text 4</label>
+                                <input type="text" id="text4" className="w50"/>
+                            </div>
+                            <div className="row align-center justify-start w50">
+                                <label className="w30" htmlFor="text5">Text 5</label>
+                                <input type="text" id="text5" className=""/>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <label htmlFor="textarea1">Text Area 1</label>
+                            <input type="textarea" id="textarea1" />
+                        </div>
+                    </div>
+                </fieldset>
+            </form>
         )
     }
 }
